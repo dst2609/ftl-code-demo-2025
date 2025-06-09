ae8ea35d9b24a3370e77a0251183f12126b9e417
Devarsh Thaker <devarsht@gmail.com>
2025-06-09 13:50:23 -0700

    eventListerner example when button is clicked

--- week2/w2d1/script.js
+++ week2/w2d1/script.js
@@ -1,3 +1,17 @@
 // Select button1 using getElementById
 const button1 = document.getElementById("button1");
 console.log("Button 1 selected:", button1);
+
+// Define an arrow function to be excuted when button 1 is clicked
+const showMessage1 = () => {
+  console.log("Button 1 is clicked");
+  const messageElement = document.querySelector("#message");
+  console.log("Message element selected using querySelector: ", messageElement);
+
+  // change the innerText of the messageElement
+  messageElement.innerText =
+    "Button 1 was clicked! Horray! we just learned event listeneres.";
+};
+
+// Register the event listeneres for the button
+button1.addEventListener("click", showMessage1);
