199a42ec22da6c57283935ab608239a597f1669d
Devarsh Thaker <devarsht@gmail.com>
2025-06-20 13:36:22 -0700

    morgan middleware

--- week3/w3d5/pokemon-db-example/server.js
+++ week3/w3d5/pokemon-db-example/server.js
@@ -1,9 +1,11 @@
 require("dotenv").config(); // lets us use .env variables in our code
 
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const pokemonRoutes = require("./routes/pokemonRoutes");
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use("/pokemon", pokemonRoutes);
 
